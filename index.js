17571016ade048bd0d2b2b3036f0845d83b8dc70
MD ANAM E RASUL CHOWDHURAY <46762842+anamerasul@users.noreply.github.com>
2024-07-14 18:31:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
-app.use(cors())
+// app.use(cors())
+
+app.use(
+  cors({
+    origin: [
+      "https://sparkfilter-client.vercel.app"
+      
+    ]
+  })
+);
 app.use(express.json())
 
 
